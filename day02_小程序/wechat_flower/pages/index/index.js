9ba5c3f73fa63aee703a240095b33ce5630b93e1
liuyuanbo <1131107655@qq.com>
2023-08-06 18:27:37 +0800

    flex布局完成主页

--- day02_小程序/wechat_flower/pages/index/index.js
+++ day02_小程序/wechat_flower/pages/index/index.js
@@ -1,2 +1,20 @@
 // index.js
-Page({})
+Page({
+    onLoad(options){
+        wx.request({
+            url: 'https://gmall-prod.atguigu.cn/mall-api/index/findBanner',
+            data: {},
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            // header: {}, // 设置请求的 header
+            success: function(res){
+                console.log(res);
+            },
+            fail: function(e) {
+                console.log(e);
+            },
+            complete: function() {
+                // complete
+            }
+        })
+    }
+})
