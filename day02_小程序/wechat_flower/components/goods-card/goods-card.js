6a3599244bb359d2bb183b9fc946db69483f514b
liuyuanbo <1131107655@qq.com>
2023-08-07 14:33:36 +0800

    商品列表页goodsList数据动态展示

---
+++ day02_小程序/wechat_flower/components/goods-card/goods-card.js
@@ -0,0 +1,26 @@
+// components/goods-card/goods-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    goodsDetail: {
+      type: Object,
+      value: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- day02_小程序/wechat_flower/pages/index/index.js
+++ day02_小程序/wechat_flower/pages/index/index.js
@@ -1,13 +1,14 @@
 // index.js
 // 在微信小程序中，Page 是一个函数，它用于注册小程序中的一个页面。
 
-const { findBanner, findCategory1 } = require("../../utils/api")
+const { findBanner, findCategory1, findListGoods } = require("../../utils/api")
 
 //Page 函数接受一个 Object 类型的参数，该参数指定页面的初始数据、生命周期回调、事件处理函数等
 Page({
     data:{
-        bannerList:[],
-        navList:[]
+        bannerList:[],// 轮播图
+        navList:[],// 导航列表
+        loveList:[],//猜你喜欢
     },
     // 在微信小程序中，onLoad 是一个页面生命周期函数，
     //它表示页面加载时触发。当页面初始化时，onLoad 函数会被调用，
@@ -31,12 +32,14 @@ Page({
 
         this.getBanners()
         this.getNavList()
+        this.getLoveList()
     },
+    /* 获取主页轮播图的功能函数 */
     async getBanners(){
         try {
             let result = await findBanner()
             if(result.code === 200){
-                this.setData({
+                this.setData({// 更新data中的数据
                     bannerList:result.data
                 })
             }
@@ -44,6 +47,7 @@ Page({
             console.log(error);
         }
     },
+    /* 获取主页导航列表的功能函数 */
     async getNavList (){
         try {
             let {data} = await findCategory1()
@@ -51,6 +55,19 @@ Page({
             this.setData({
                 navList:data
             })
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    /* 获取猜你喜欢的功能函数 */
+    async getLoveList (){
+        try {
+            let {code,data} = await findListGoods()
+            if(code === 200){
+                this.setData({
+                    loveList:data
+                })
+            }
         } catch (error) {
             console.log(error);
         }
